7c624f138a206427548cfe05356972bd5b4262a6
ejuerg <ejuerg@gmail.com>
2014-03-18 20:56:21 +0100

    Add unit tests for sorting by angle

--- src/algorithm/structure/test/NodePointCollectionTest.java
+++ src/algorithm/structure/test/NodePointCollectionTest.java
@@ -1,16 +1,27 @@
 package algorithm.structure.test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import com.vividsolutions.jts.geom.Coordinate;
+
+import algorithm.structure.NodePoint;
+import algorithm.structure.NodePointCollection;
+
 public class NodePointCollectionTest
 {
+
+	private NodePointCollection nodePointCollection;
+	private Coordinate origin;
 
 	@Before
 	public void setUp() throws Exception
 	{
+		origin = new Coordinate(0,0);
 	}
 
 	@Test
@@ -38,9 +49,30 @@ public class NodePointCollectionTest
 	}
 
 	@Test
+	public void testSortByAngleAlreadySorted()
+	{
+		NodePoint[] nodePoints = new NodePoint[] {new NodePoint(1,0), new NodePoint(1,1), new NodePoint(0,1)};
+		nodePointCollection = new NodePointCollection(nodePoints);
+		
+		nodePointCollection = nodePointCollection.sortByAngle(origin);
+		
+		assertEquals(nodePoints[0], nodePointCollection.get(0));
+		assertEquals(nodePoints[1], nodePointCollection.get(1));
+		assertEquals(nodePoints[2], nodePointCollection.get(2));
+	}
+	
+	@Test
 	public void testSortByAngle()
 	{
-		fail("Not yet implemented");
+		NodePoint[] nodePoints = new NodePoint[] {new NodePoint(-1,0), new NodePoint(1,1), new NodePoint(0,1), new NodePoint(-1,-1)};
+		nodePointCollection = new NodePointCollection(nodePoints);
+		
+		nodePointCollection = nodePointCollection.sortByAngle(origin);
+		
+		assertEquals(nodePoints[3], nodePointCollection.get(0));
+		assertEquals(nodePoints[1], nodePointCollection.get(1));
+		assertEquals(nodePoints[2], nodePointCollection.get(2));
+		assertEquals(nodePoints[0], nodePointCollection.get(3));		
 	}
 
 	@Test
