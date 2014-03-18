7b9444fc6877d93af101c7ba591056c8e331b124
ejuerg <ejuerg@gmail.com>
2014-03-18 21:10:58 +0100

    Increase test readability

--- src/algorithm/structure/test/NodePointCollectionTest.java
+++ src/algorithm/structure/test/NodePointCollectionTest.java
@@ -3,7 +3,6 @@ package algorithm.structure.test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import algorithm.structure.NodePoint;
@@ -15,13 +14,12 @@ public class NodePointCollectionTest
 {
 
 	private NodePointCollection nodePointCollection;
-	private Coordinate origin;
-
-	@Before
-	public void setUp() throws Exception
-	{
-		origin = new Coordinate(0, 0);
-	}
+	private final Coordinate origin = new Coordinate(0, 0);
+	private final NodePoint nodeAtDeg0 = new NodePoint(1, 0);
+	private final NodePoint nodeAtDeg180 = new NodePoint(-1, 0);
+	private final NodePoint nodeAtDeg45 = new NodePoint(1, 1);
+	private final NodePoint nodeAtDegMinus135 = new NodePoint(-1, -1);
+	private final NodePoint nodeAtDeg90 = new NodePoint(0, 1);
 
 	@Test
 	public void testNodePointCollection()
@@ -50,29 +48,28 @@ public class NodePointCollectionTest
 	@Test
 	public void testSortByAngleAlreadySorted()
 	{
-		NodePoint[] nodePoints = new NodePoint[] { new NodePoint(1, 0), new NodePoint(1, 1), new NodePoint(0, 1) };
+		NodePoint[] nodePoints = new NodePoint[] { nodeAtDeg0, nodeAtDeg45, nodeAtDeg90 };
 		nodePointCollection = new NodePointCollection(nodePoints);
 
 		nodePointCollection = nodePointCollection.sortByAngle(origin);
 
-		assertEquals(nodePoints[0], nodePointCollection.get(0));
-		assertEquals(nodePoints[1], nodePointCollection.get(1));
-		assertEquals(nodePoints[2], nodePointCollection.get(2));
+		assertEquals(nodeAtDeg0, nodePointCollection.get(0));
+		assertEquals(nodeAtDeg45, nodePointCollection.get(1));
+		assertEquals(nodeAtDeg90, nodePointCollection.get(2));
 	}
 
 	@Test
 	public void testSortByAngle()
 	{
-		NodePoint[] nodePoints = new NodePoint[] { new NodePoint(-1, 0), new NodePoint(1, 1), new NodePoint(0, 1),
-				new NodePoint(-1, -1) };
+		NodePoint[] nodePoints = new NodePoint[] { nodeAtDeg180, nodeAtDeg45, nodeAtDeg90, nodeAtDegMinus135 };
 		nodePointCollection = new NodePointCollection(nodePoints);
 
 		nodePointCollection = nodePointCollection.sortByAngle(origin);
 
-		assertEquals(nodePoints[3], nodePointCollection.get(0));
-		assertEquals(nodePoints[1], nodePointCollection.get(1));
-		assertEquals(nodePoints[2], nodePointCollection.get(2));
-		assertEquals(nodePoints[0], nodePointCollection.get(3));
+		assertEquals(nodeAtDegMinus135, nodePointCollection.get(0));
+		assertEquals(nodeAtDeg45, nodePointCollection.get(1));
+		assertEquals(nodeAtDeg90, nodePointCollection.get(2));
+		assertEquals(nodeAtDeg180, nodePointCollection.get(3));
 	}
 
 	@Test
@@ -83,12 +80,6 @@ public class NodePointCollectionTest
 
 	@Test
 	public void testSortByDistance()
-	{
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGet()
 	{
 		fail("Not yet implemented");
 	}
